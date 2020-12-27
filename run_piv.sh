d9cf6185a86c6ea2872236d6b37673acb2a40921
AsumilP <tkmr.alamode725@gmail.com>
2020-12-27 14:39:50 +0900

    Update run_piv.sh

--- run_piv.sh
+++ run_piv.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-bsub -J "ATA" -o "stdo%J.txt" -n 1 "$LSF_BINDIR/openmpi_wrapper ./piv_20190301fr.sh"
+bsub -J "ATA" -o "stdo%J.txt" -n 1 "$LSF_BINDIR/openmpi_wrapper ./piv_20201223fr.sh"
