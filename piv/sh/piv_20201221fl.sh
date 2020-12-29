8b370e136bd4b0c365027b2b969f6da5e8c39675
AsumilP <tkmr.alamode725@gmail.com>
2020-12-29 12:37:25 +0900

    Create piv_20201221fl.sh

---
+++ piv/sh/piv_20201221fl.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+./piv -s 1 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_01_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_01_velo1.dat &
+./piv -s 1 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_02_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_02_velo1.dat &
+./piv -s 1 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_04_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_04_velo1.dat &
+./piv -s 1 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_05_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_05_velo1.dat &
+./piv -s 1 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_06_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_06_velo1.dat &
+./piv -s 1 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_07_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_07_velo1.dat &
+wait
+
+./piv -s 2001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_01_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_01_velo2.dat &
+./piv -s 2001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_02_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_02_velo2.dat &
+./piv -s 2001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_04_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_04_velo2.dat &
+./piv -s 2001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_05_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_05_velo2.dat &
+./piv -s 2001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_06_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_06_velo2.dat &
+./piv -s 2001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_07_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_07_velo2.dat &
+wait
+
+./piv -s 4001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_01_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_01_velo2.dat &
+./piv -s 4001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_02_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_02_velo2.dat &
+./piv -s 4001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_04_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_04_velo2.dat &
+./piv -s 4001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_05_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_05_velo2.dat &
+./piv -s 4001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_06_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_06_velo2.dat &
+./piv -s 4001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_07_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_07_velo2.dat &
+wait
+
+./piv -s 6001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_01_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_01_velo2.dat &
+./piv -s 6001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_02_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_02_velo2.dat &
+./piv -s 6001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_04_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_04_velo2.dat &
+./piv -s 6001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_05_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_05_velo2.dat &
+./piv -s 6001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_06_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_06_velo2.dat &
+./piv -s 6001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_07_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_07_velo2.dat &
+wait
+
+./piv -s 8001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_01_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_01_velo2.dat &
+./piv -s 8001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_02_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_02_velo2.dat &
+./piv -s 8001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_04_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_04_velo2.dat &
+./piv -s 8001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_05_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_05_velo2.dat &
+./piv -s 8001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_06_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_06_velo2.dat &
+./piv -s 8001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_07_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_07_velo2.dat &
+wait
+
+./piv -s 10001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_01_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_01_velo2.dat &
+./piv -s 10001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_02_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_02_velo2.dat &
+./piv -s 10001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_04_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_04_velo2.dat &
+./piv -s 10001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_05_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_05_velo2.dat &
+./piv -s 10001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_06_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_06_velo2.dat &
+./piv -s 10001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_07_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_07_velo2.dat &
+wait
+
+./piv -s 12001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_01_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_01_velo2.dat &
+./piv -s 12001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_02_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_02_velo2.dat &
+./piv -s 12001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_04_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_04_velo2.dat &
+./piv -s 12001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_05_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_05_velo2.dat &
+./piv -s 12001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_06_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_06_velo2.dat &
+./piv -s 12001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_07_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_07_velo2.dat &
+wait
+
+./piv -s 14001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_01_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_01_velo2.dat &
+./piv -s 14001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_02_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_02_velo2.dat &
+./piv -s 14001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_04_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_04_velo2.dat &
+./piv -s 14001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_05_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_05_velo2.dat &
+./piv -s 14001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_06_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_06_velo2.dat &
+./piv -s 14001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_07_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_07_velo2.dat &
+wait
+
+./piv -s 16001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_01_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_01_velo2.dat &
+./piv -s 16001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_02_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_02_velo2.dat &
+./piv -s 16001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_04_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_04_velo2.dat &
+./piv -s 16001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_05_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_05_velo2.dat &
+./piv -s 16001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_06_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_06_velo2.dat &
+./piv -s 16001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_07_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_07_velo2.dat &
+wait
+
+./piv -s 18001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_01_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_01_velo2.dat &
+./piv -s 18001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_02_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_02_velo2.dat &
+./piv -s 18001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_04_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_04_velo2.dat &
+./piv -s 18001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_05_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_05_velo2.dat &
+./piv -s 18001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_06_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_06_velo2.dat &
+./piv -s 18001 -n 2000 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_07_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_07_velo2.dat &
+wait
+
+./piv -s 20001 -n 1838 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_01_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_01_velo2.dat &
+./piv -s 20001 -n 1838 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_02_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_02_velo2.dat &
+./piv -s 20001 -n 1838 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_04_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_04_velo2.dat &
+./piv -s 20001 -n 1838 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_05_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_05_velo2.dat &
+./piv -s 20001 -n 1838 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_06_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_06_velo2.dat &
+./piv -s 20001 -n 1838 --dt 50 -i /home/yatagi/analysis/piv_output/flow_cor/20201221/fl/spiv_fl_07_cor.dat -o /home/yatagi/analysis/piv_output/velofield/20201221/fl/spiv_fl_07_velo2.dat &
+wait
