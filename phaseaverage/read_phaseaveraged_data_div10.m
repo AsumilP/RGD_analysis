79808b04e4c43bba6db4b9085e0a0fe79bbd6f81
AsumilP <tkmr.alamode725@gmail.com>
2021-01-25 21:05:00 +0900

    Update read_phaseaveraged_data_div10.m

--- phaseaverage/read_phaseaveraged_data_div10.m
+++ phaseaverage/read_phaseaveraged_data_div10.m
@@ -16,7 +16,7 @@ function [data_av] = read_phaseaveraged_data_div10(flow_rate,series,ndata,nx,ny,
       end
 
   elseif mode == 2
-      formatspec=strcat(dir_i,'phasemean_',series,'_%d_',date,'_%d.dat');
+      formatspec=strcat(dir_i,'phasemean_',series,'_%d_',date,'_%02u.dat');
       fid=fopen(sprintf(formatspec,flow_rate,cond),'r');
       for j=1:1:10
           I = fread(fid,nx*ny,'double');
