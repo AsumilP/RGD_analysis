f541522f0dc1d9fbd8b7e9954650ef1a598586b1
AsumilP <tkmr.alamode725@gmail.com>
2021-01-25 17:12:12 +0900

    Update u_v_w_velo_image_png.m

--- visualize/u_v_w_velo_image_png.m
+++ visualize/u_v_w_velo_image_png.m
@@ -6,7 +6,7 @@
 
     nincrement=2;
     quivermode=2;
-    
+
     date=20201223;
     num=7;
     first_frame=1;
@@ -14,7 +14,7 @@
     skip_frame=0;
     wmin = -5; % [m/s]
     wmax = 5; % [m/s]
-    
+
     nxall=191;
     nyall=123;
     nzall=21838;
@@ -28,7 +28,7 @@
     img_res_y=75*10^(-3); % [mm]
     dx=img_res_x*vec_spc_x;
     dy=img_res_y*vec_spc_y;
-    
+
     dir=sprintf('C:/Users/yatagi/Desktop/work/piv_output/%d/average/combined/',date);
     file_u=sprintf('spiv_fbsc_%02u_u_av.dat',num);
     file_v=sprintf('spiv_fbsc_%02u_v_av.dat',num);
@@ -49,7 +49,7 @@
     fig=figure;
     fig.Color='white';
     fig.Position=[1 1 800*(1+sqrt(5))/2 800];
-    
+
     if first_frame + (skip_frame+1) * frames <= nzall
 
         for idx=1:1:frames
@@ -57,18 +57,17 @@
             v = (fread(fid2,nxall*nyall,'double'));
             w = (fread(fid3,nxall*nyall,'double'));
             squiver(u,-v,w,nxall,nyall,nx_start,nx_end,ny_start,ny_end,nincrement,...
-                         dx,dy,wmin,wmax,quivermode,idx,skip_frame)
+                         dx,dy,wmin,wmax,quivermode)
             skip_frames(fid1,skip_frame,nxall*nyall,8,2);
             skip_frames(fid2,skip_frame,nxall*nyall,8,2);
             skip_frames(fid3,skip_frame,nxall*nyall,8,2);
-            
+
             fign=strcat(pngname,sprintf('_%d',first_frame+(idx-1)*(skip_frame+1)));
             saveas(gcf, strcat(dir,fign,figex));
-            
+
         end
-        
+
     else
         disp('Change frames or skip_frame!')
         first_frame + (skip_frame+1) * frames
-    end           
- 
+    end
