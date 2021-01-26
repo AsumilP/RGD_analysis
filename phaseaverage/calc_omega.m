3b9bfdf885a54b421adb35110f6088affd55157c
AsumilP <tkmr.alamode725@gmail.com>
2021-01-27 02:07:53 +0900

    Update calc_omega.m

--- phaseaverage/calc_omega.m
+++ phaseaverage/calc_omega.m
@@ -1,5 +1,13 @@
-function [omega] = calc_omega(dvdx,dudy)
+function [omega_o] = calc_omega(u,v,dx,dy,nx,ny)
+
+    N = [nx ny];
+    u = permute(reshape(u,N),[2 1]);
+    v = permute(reshape(v,N),[2 1]);
+
+    [dudx, dudy] = 4th_order_dudx(u,dx,dy,nx,ny)
+    [dvdx, dvdy] = 4th_order_dudx(v,dx,dy,nx,ny)
 
     omega=dvdx-dudy;
+    omega_o=reshape(omega,[nx*ny 1]);
 
 end
