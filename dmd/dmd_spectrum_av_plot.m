414c53e56bc87a50f3df24a89a710ff94d35daca
AsumilP <tkmr.alamode725@gmail.com>
2021-01-31 12:41:56 +0900

    Update dmd_spectrum_av_plot.m

--- dmd/dmd_spectrum_av_plot.m
+++ dmd/dmd_spectrum_av_plot.m
@@ -4,27 +4,27 @@
 
 %% PARAMETERS
 
-    flow_rate1 = 400; % [L/min]
-    equivalence_ratio1 = 0;
+    flow_rate1 = 500; % [L/min]
+    equivalence_ratio1 = 0.80;
     specific_f1 = 0;
-    sw1 = 45;
-    duct_l1 = 1185;
+    sw1 = 60;
+    duct_l1 = 582;
 %     step1 = 'trans1';
     
-    flow_rate2 = 450; % [L/min]
-    equivalence_ratio2 = 0;
-    specific_f2 = 0;
-    sw2 = 45;
-    duct_l2 = 1185;
+    flow_rate2 = 500; % [L/min]
+    equivalence_ratio2 = 0.80;
+    specific_f2 = 146;
+    sw2 = 60;
+    duct_l2 = 582;
 %     step2 = 'trans2';
     
-%     flow_rate3 = 500; % [L/min]
-%     equivalence_ratio3 = 0;
-%     specific_f3 = 68;
-%     sw3 = 45;
-%     duct_l3 = 1185;
-% %     step3 = 'trans3';
-%     
+    flow_rate3 = 500; % [L/min]
+    equivalence_ratio3 = 0.80;
+    specific_f3 = 189;
+    sw3 = 60;
+    duct_l3 = 582;
+%     step3 = 'trans3';
+    
 %     flow_rate4 = 500; % [L/min]
 %     equivalence_ratio4 = 0;
 %     specific_f4 = 64;
@@ -46,11 +46,11 @@
 %     duct_l6 = 582;
 % %     step6 = 'trans6';
     
-    dir = 'I:/Analysis/piv_output/dmd/dmd_averaged/';
+    dir = 'G:/chem_dmd/dmd_averaged/';
 %     dir = 'D:/Analysis/chem_output/chem_dmd/dmd_averaged/';
 %     type = 'u';
-    type = 'v';
-%     type = 'chem';
+%     type = 'v';
+    type = 'chem';
 
 %% READ
 
@@ -71,8 +71,8 @@
     norm_dm_fil_in1 = sprintf(strcat(dir,'norm_dm_fil_%02u_%.2f_%dHz_sw%d_d%d_',type,'.dat'),flow_rate1,equivalence_ratio1,specific_f1,sw1,duct_l1);
     fg_m_fil_in2 = sprintf(strcat(dir,'fg_m_real_fil_%02u_%.2f_%dHz_sw%d_d%d_',type,'.dat'),flow_rate2,equivalence_ratio2,specific_f2,sw2,duct_l2);
     norm_dm_fil_in2 = sprintf(strcat(dir,'norm_dm_fil_%02u_%.2f_%dHz_sw%d_d%d_',type,'.dat'),flow_rate2,equivalence_ratio2,specific_f2,sw2,duct_l2);
-%     fg_m_fil_in3 = sprintf(strcat(dir,'fg_m_real_fil_%02u_%.2f_%dHz_sw%d_d%d_',type,'.dat'),flow_rate3,equivalence_ratio3,specific_f3,sw3,duct_l3);
-%     norm_dm_fil_in3 = sprintf(strcat(dir,'norm_dm_fil_%02u_%.2f_%dHz_sw%d_d%d_',type,'.dat'),flow_rate3,equivalence_ratio3,specific_f3,sw3,duct_l3);
+    fg_m_fil_in3 = sprintf(strcat(dir,'fg_m_real_fil_%02u_%.2f_%dHz_sw%d_d%d_',type,'.dat'),flow_rate3,equivalence_ratio3,specific_f3,sw3,duct_l3);
+    norm_dm_fil_in3 = sprintf(strcat(dir,'norm_dm_fil_%02u_%.2f_%dHz_sw%d_d%d_',type,'.dat'),flow_rate3,equivalence_ratio3,specific_f3,sw3,duct_l3);
 %     fg_m_fil_in4 = sprintf(strcat(dir,'fg_m_real_fil_%02u_%.2f_%dHz_sw%d_d%d_',type,'.dat'),flow_rate4,equivalence_ratio4,specific_f4,sw4,duct_l4);
 %     norm_dm_fil_in4 = sprintf(strcat(dir,'norm_dm_fil_%02u_%.2f_%dHz_sw%d_d%d_',type,'.dat'),flow_rate4,equivalence_ratio4,specific_f4,sw4,duct_l4);
 %     fg_m_fil_in5 = sprintf(strcat(dir,'fg_m_real_fil_%02u_%.2f_%dHz_sw%d_d%d_',type,'.dat'),flow_rate5,equivalence_ratio5,specific_f5,sw5,duct_l5);
@@ -90,10 +90,10 @@
     fg2 = fread(fileID,[1 inf],'double');
     fclose(fileID);
     
-%     fileID = fopen(fg_m_fil_in3,'r');
-%     fg3 = fread(fileID,[1 inf],'double');
-%     fclose(fileID);
-%     
+    fileID = fopen(fg_m_fil_in3,'r');
+    fg3 = fread(fileID,[1 inf],'double');
+    fclose(fileID);
+    
 %     fileID = fopen(fg_m_fil_in4,'r');
 %     fg4 = fread(fileID,[1 inf],'double');
 %     fclose(fileID);
@@ -114,10 +114,10 @@
     norm2 = fread(fileID,[1 inf],'double');
     fclose(fileID);
     
-%     fileID = fopen(norm_dm_fil_in3,'r');
-%     norm3 = fread(fileID,[1 inf],'double');
-%     fclose(fileID);
-%     
+    fileID = fopen(norm_dm_fil_in3,'r');
+    norm3 = fread(fileID,[1 inf],'double');
+    fclose(fileID);
+    
 %     fileID = fopen(norm_dm_fil_in4,'r');
 %     norm4 = fread(fileID,[1 inf],'double');
 %     fclose(fileID);
@@ -134,7 +134,7 @@
 
     figure('Position', [50 50 960 735],'Color','white');
 %     loglog(fg1,norm1,'-vr','MarkerSize',8,'MarkerFaceColor','w','MarkerIndices',1:1:length(norm1))
-    loglog(fg1,norm1,'-xk','MarkerSize',8,'MarkerFaceColor','w','MarkerIndices',1:1:length(norm1))
+    loglog(fg2,norm2,'-pr','MarkerSize',8,'MarkerFaceColor','w','MarkerIndices',1:1:length(norm1))
 %     loglog(fg1,norm1,'-pr','MarkerSize',8,'MarkerFaceColor','w','MarkerIndices',1:1:length(norm1))
 
     ax = gca;
@@ -154,9 +154,9 @@
      ax.YScale = 'log';
      ax.XLim = [20 400];
 %      ax.YLim = [4 200];
-%      ax.YLim = [10^3 2*10^5];
+     ax.YLim = [10^3 2*10^4];
 %      ax.YLim = [2 20];
-     ax.YLim = [1 20];
+%      ax.YLim = [1 20];
      ax.FontSize = 20;
      ax.FontName = 'Times New Roman';
      ax.TitleFontSizeMultiplier = 2;
@@ -170,11 +170,11 @@
      hold on
      
 %      loglog(fg2,norm2,'-sb','MarkerSize',8,'MarkerFaceColor','w','MarkerIndices',1:1:length(norm2))
-     loglog(fg2,norm2,'-pr','MarkerSize',8,'MarkerFaceColor','w','MarkerIndices',1:1:length(norm2))
+     loglog(fg3,norm3,'-xb','MarkerSize',8,'MarkerFaceColor','w','MarkerIndices',1:1:length(norm2))
 %      loglog(fg2,norm2,'-xk','MarkerSize',8,'MarkerFaceColor','w','MarkerIndices',1:1:length(norm2))
      hold on
      
-%      loglog(fg3,norm3,'-dk','MarkerSize',8,'MarkerFaceColor','w','MarkerIndices',1:1:length(norm3))
+     loglog(fg1,norm1,'-dk','MarkerSize',8,'MarkerFaceColor','w','MarkerIndices',1:1:length(norm3))
 %      loglog(fg3,norm3,'-og','MarkerSize',8,'MarkerFaceColor','w','MarkerIndices',1:1:length(norm3))
      hold on
      
@@ -201,7 +201,7 @@
 %      legend('156Hz','FontSize',16,'Location','northwest')
 %      legend('189Hz','No excitation','FontSize',16,'Location','northwest')
 %      legend('500L/min','450L/min','400L/min','250L/min','FontSize',16,'Location','northwest')
-     legend('400L/min','450L/min','FontSize',16,'Location','northwest')
+     legend('146Hz','189Hz','No excitaion','FontSize',16,'Location','northwest')
 %      legend('500L/min, 0.66, 85Hz','400L/min, 0.66, 85Hz','FontSize',16,'Location','northwest')
      hold off
      pbaspect([sqrt(2) 1 1]);
